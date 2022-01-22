5fef7a7b100ab609c0a94a1ecaf38fff15cc14fd
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-23 01:20:10 +0530

    Update alive.py

--- Ub/plugins/alive.py
+++ Ub/plugins/alive.py
@@ -3,15 +3,26 @@ from config import HNDLR
 import psutil
 from psutil._common import bytes2human
 from pyrogram import filters
-from pyrogram import Client blaze
+from pyrogram import Client as blaze
 
-ALIVE_IMG = "https://telegra.ph/file/a8fff8cafaf00ee379280.jpg"
+ALIVE_IMG = "https://telegra.ph/file/a8fff8cafaf00ee379280.jpg", 
+
+keyboard = InlineKeyboardMarkup(
+ [
+    [
+        InlineKeyboardButton("Gʀᴏᴜᴘ", url="t.me/xraichu_official"),
+        InlineKeyboardButton("Cʜᴀɴɴᴇʟ", url="t.me/xraichunews")
+    ]
+ ]
+) 
 
 @blaze.on_message(filters.command(["alive"], prefixes=f"{HNDLR}"))
 @authorized_users_only
 async def get_sysinfo(client, m):
-    response = "**Raichu Userbot Is Active And On Fire🔥**:\n"
+    response = "**Raichu Userbot Is Active And On Fire🔥**"
     m_reply = await m.reply_photo(
                                  photo={ALIVE_IMG},
-                                 captio={response}
+                                 reply_markup=keyboard,
+                                 captio={response}, 
+     
  ) 
