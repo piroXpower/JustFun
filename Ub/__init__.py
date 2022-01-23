f9c61fa8dd5d4dfb7648f8cde88b40f800511e57
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-23 11:27:38 +0530

    Update __init__.py

--- Ub/__init__.py
+++ Ub/__init__.py
@@ -1,9 +1,12 @@
 ## For Future
 from datetime import datetime
 import time
+from telethon import TelegramClient
+from config import STRING_SESSION, API_ID, API_HASH
 
 starttimer=datetime.now()
 START_TIME = datetime.now()
 
+ubot = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 
 print("DATE AND TIME LOADED") 
